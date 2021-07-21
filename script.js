087dcebd0c2f6a4e023cf19be263c0b27d6c1c4f
V1adimirVoronin <87757335+V1adimirVoronin@users.noreply.github.com>
2021-07-21 17:40:19 +0500

    Add files via upload
    script for dots

--- script.js
+++ script.js
@@ -6,9 +6,53 @@ function getCoords(elem) {
         return box;
 }
 
-let sectionCoord = getCoords(document.getElementById('section1'));
+let actDotPosicion = 'dot1';
 
 document.getElementById('scroller').onscroll = function () {
-        console.log(sectionCoord);
+        let sectionCoord = getCoords(document.getElementById('section1'));
+        if (sectionCoord == 5021) {
+                let a = document.getElementById('dot1');
+                a.src = 'pic/active-dot.png';
+                let b = document.getElementById('active-dot');
+                b.src = 'pic/dot.png';
+                b.id = actDotPosicion;
+                actDotPosicion = a.id;
+                a.id = 'active-dot';
+        };
+        if (sectionCoord == 4656) {
+                let a = document.getElementById('dot2');
+                a.src = 'pic/active-dot.png';
+                let b = document.getElementById('active-dot');
+                b.src = 'pic/dot.png';
+                b.id = actDotPosicion;
+                actDotPosicion = a.id;
+                a.id = 'active-dot';
+        };
+        if (sectionCoord == 4291) {
+                let a = document.getElementById('dot3');
+                a.src = 'pic/active-dot.png';
+                let b = document.getElementById('active-dot');
+                b.src = 'pic/dot.png';
+                b.id = actDotPosicion;
+                actDotPosicion = a.id;
+                a.id = 'active-dot';
+        };
+        if (sectionCoord == 3926) {
+                let a = document.getElementById('dot4');
+                a.src = 'pic/active-dot.png';
+                let b = document.getElementById('active-dot');
+                b.src = 'pic/dot.png';
+                b.id = actDotPosicion;
+                actDotPosicion = a.id;
+                a.id = 'active-dot';
+        };
+        if (sectionCoord == 3561) {
+                let a = document.getElementById('dot5');
+                a.src = 'pic/active-dot.png';
+                let b = document.getElementById('active-dot');
+                b.src = 'pic/dot.png';
+                b.id = actDotPosicion;
+                actDotPosicion = a.id;
+                a.id = 'active-dot';
+        };
 }
-365
