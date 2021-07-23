c31a591c5b1c9f0d10026278e00ea618c3e3dcf9
V1adimirVoronin <87757335+V1adimirVoronin@users.noreply.github.com>
2021-07-23 19:47:09 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,7 +11,17 @@ let startActDotPosicion = getCoords(document.getElementById('section1'));
 
 document.getElementById('scroller').onscroll = function () {
         let sectionCoord = getCoords(document.getElementById('section1'));
-        if (sectionCoord == startActDotPosicion-365) {
+        if (sectionCoord == startActDotPosicion) {
+                let a = document.getElementById('dot1');
+                if (a != null) {
+                        a.src = 'pic/active-dot.png';
+                        let b = document.getElementById('active-dot');
+                        b.src = 'pic/dot.png';
+                        b.id = actDotPosicion;
+                        actDotPosicion = a.id;
+                        a.id = 'active-dot';
+                };
+        } else if (sectionCoord == startActDotPosicion-365) {
                 let a = document.getElementById('dot2');
                 a.src = 'pic/active-dot.png';
                 let b = document.getElementById('active-dot');
@@ -37,14 +47,6 @@ document.getElementById('scroller').onscroll = function () {
                 a.id = 'active-dot';
         } else if (sectionCoord == startActDotPosicion-365*4) {
                 let a = document.getElementById('dot5');
-                a.src = 'pic/active-dot.png';
-                let b = document.getElementById('active-dot');
-                b.src = 'pic/dot.png';
-                b.id = actDotPosicion;
-                actDotPosicion = a.id;
-                a.id = 'active-dot';
-        } else if (sectionCoord == startActDotPosicion) {
-                let a = document.getElementById('dot1');
                 a.src = 'pic/active-dot.png';
                 let b = document.getElementById('active-dot');
                 b.src = 'pic/dot.png';
