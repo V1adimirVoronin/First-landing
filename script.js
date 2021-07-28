b719dea81752ee64b972a6b514a5a6562fdebd2f
V1adimirVoronin <87757335+V1adimirVoronin@users.noreply.github.com>
2021-07-28 12:39:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-'use strict';
+'use strict'
 
 function getCoords(elem) {
         let box = elem.getBoundingClientRect().top + pageYOffset;
         return Math.trunc(box);
 }
 
+function dotMove() {
+        let a = document.getElementById(n);
+        if (a != null) {
+            a.src = 'pic/active-dot.png';
+            let b = document.getElementById('active-dot');
+            b.src = 'pic/dot.png';
+            b.id = actDotPosicion;
+            actDotPosicion = a.id;
+            a.id = 'active-dot';
+        };
+}
+
 let actDotPosicion = 'dot1';
 let sectionList = document.querySelectorAll('div.section');
 let startActDotPosicion = getCoords(sectionList[0]);
 let sectionMove = getCoords(sectionList[1])-startActDotPosicion;
+let n;
 
 document.getElementById('scroller').onscroll = function () {
         let sectionCoord = getCoords(sectionList[0]);
         if (sectionCoord == startActDotPosicion) {
-                let a = document.getElementById('dot1');
-                if (a != null) {
-                        a.src = 'pic/active-dot.png';
-                        let b = document.getElementById('active-dot');
-                        b.src = 'pic/dot.png';
-                        b.id = actDotPosicion;
-                        actDotPosicion = a.id;
-                        a.id = 'active-dot';
-                };
+                n = 'dot1'
         } else if (sectionCoord == startActDotPosicion-sectionMove) {
-                let a = document.getElementById('dot2');
-                a.src = 'pic/active-dot.png';
-                let b = document.getElementById('active-dot');
-                b.src = 'pic/dot.png';
-                b.id = actDotPosicion;
-                actDotPosicion = a.id;
-                a.id = 'active-dot';
+                n = 'dot2'
         } else if (sectionCoord == startActDotPosicion-sectionMove*2) {
-                let a = document.getElementById('dot3');
-                a.src = 'pic/active-dot.png';
-                let b = document.getElementById('active-dot');
-                b.src = 'pic/dot.png';
-                b.id = actDotPosicion;
-                actDotPosicion = a.id;
-                a.id = 'active-dot';
+                n = 'dot3'
         } else if (sectionCoord == startActDotPosicion-sectionMove*3) {
-                let a = document.getElementById('dot4');
-                a.src = 'pic/active-dot.png';
-                let b = document.getElementById('active-dot');
-                b.src = 'pic/dot.png';
-                b.id = actDotPosicion;
-                actDotPosicion = a.id;
-                a.id = 'active-dot';
+                n = 'dot4'
         } else if (sectionCoord == startActDotPosicion-sectionMove*4) {
-                let a = document.getElementById('dot5');
-                if (a != null) {
-                        a.src = 'pic/active-dot.png';
-                        let b = document.getElementById('active-dot');
-                        b.src = 'pic/dot.png';
-                        b.id = actDotPosicion;
-                        actDotPosicion = a.id;
-                        a.id = 'active-dot';
-                };
+                n = 'dot5'
         };
-}
+        dotMove();
+};
